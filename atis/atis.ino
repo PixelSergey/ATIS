1f05af1e0c404b806e5341f3ad6b67bafaa62086
PixelSergey <sergey.ichtchenko@gmail.com>
2023-09-02 15:27:28 +0300

    Move audio loading to a separate function

--- atis/atis.ino
+++ atis/atis.ino
@@ -17,15 +17,26 @@ std::vector<std::string> phrase;
 int pos;
 
 
+void loadAudio() {
+  vox["A"] = new AudioFileSourcePROGMEM(A_mp3, A_mp3_len);
+  vox["B"] = new AudioFileSourcePROGMEM(B_mp3, B_mp3_len);
+  vox["C"] = new AudioFileSourcePROGMEM(C_mp3, C_mp3_len);
+  vox["THIS_IS"] = new AudioFileSourcePROGMEM(THIS_IS_mp3, THIS_IS_mp3_len);
+  vox["KUMPULA"] = new AudioFileSourcePROGMEM(KUMPULA_mp3, KUMPULA_mp3_len);
+  vox["INFORMATION"] = new AudioFileSourcePROGMEM(INFORMATION_mp3, INFORMATION_mp3_len);
+}
+
+
 void setup() {
   delay(1000);
 
-  vox["A"] = new AudioFileSourcePROGMEM(A_mp3, A_mp3_len);
-  vox["B"] = new AudioFileSourcePROGMEM(B_mp3, B_mp3_len);
   out = new AudioOutputI2SNoDAC();
   aud = new AudioGeneratorMP3();
 
-  phrase = {"A", "B"};
+  loadAudio();
+
+  int time = millis();
+  phrase = {"THIS_IS", "KUMPULA", "INFORMATION", time%3==0 ? "A" : (time%3==1 ? "B" : "C")};
   pos = 0;
 }
 
@@ -34,7 +45,6 @@ void loop() {
     if (!aud->loop()) aud->stop();
   } else {
     if (pos<phrase.size()) {
-      delay(200);
       aud->begin(vox[phrase[pos]], out);
       pos++;
     } else {
