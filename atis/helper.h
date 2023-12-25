08e5b1cc8485d94aeb1b75fb7aa7fec66deae1a4
PixelSergey <sergey.ichtchenko@gmail.com>
2023-12-25 03:23:05 +0200

    Convert phrase generation to use regex

--- atis/helper.h
+++ atis/helper.h
@@ -39,6 +39,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <regex>
 
 // Arduino library includes
 
@@ -65,6 +66,53 @@ std::map<std::string, std::pair<const unsigned char*, unsigned int>> tokenToAudi
     {"THIS_IS", {THIS_IS_mp3, THIS_IS_mp3_len}},
     {"KUMPULA", {KUMPULA_mp3, KUMPULA_mp3_len}},
     {"INFORMATION", {INFORMATION_mp3, INFORMATION_mp3_len}},
+};
+
+// This enum contains all the possible METAR tokens
+// Must be updated if the METAR standard changes or bugs are found
+enum TokenType {
+    STATION,
+    TIME,
+    NIL,
+    AUTO,
+    WIND,
+    VARIABLE,
+    VISIBILITY,
+    RVR,
+    WEATHER,
+    CLOUD,
+    NSC,
+    NCD,
+    VERTICAL,
+    TEMPERATURE,
+    QNH,
+    WINDSHEAR,
+    ALL,
+    RWY,
+    RUNWAY_NUMBER,
+};
+
+// This map must be updated if the METAR standard changes or bugs are found
+std::map<std::string, TokenType> regexToToken = {
+    {"^(IL..$|^EF..)$", STATION},
+    {"^[0-9]{2}([0-9]{4})Z$", TIME},
+    {"^NIL$", NIL},
+    {"^AUTO$", AUTO},
+    {"^(?:(\\/{5})|(VRB|[0-9]{3})([0-9]{2})(?:G([0-9]{2}))?)KT$", WIND},
+    {"^([0-9]{3})V([0-9]{3})$", VARIABLE},
+    {"^(\\/{4})|([0-9]{4})$", VISIBILITY},
+    {"^R([0-9]{2})\\/([MP]?)([0-9]{4})([UDN]?)$", RVR},
+    {"^(\\+|-|VC|RE|MI|BC|PR|DR|BL|SH|TS|FZ|DZ|RA|SN|SG|PL|GR|GS|UP|BR|FG|FU|VA|DU|SA|HZ|PO|SQ|FC|SS|DS)(\\+|-|VC|RE|MI|BC|PR|DR|BL|SH|TS|FZ|DZ|RA|SN|SG|PL|GR|GS|UP|BR|FG|FU|VA|DU|SA|HZ|PO|SQ|FC|SS|DS)?(\\+|-|VC|RE|MI|BC|PR|DR|BL|SH|TS|FZ|DZ|RA|SN|SG|PL|GR|GS|UP|BR|FG|FU|VA|DU|SA|HZ|PO|SQ|FC|SS|DS)?$", WEATHER},
+    {"^(FEW|SCT|BKN|OVC)([0-9]{3})(CB|TCU)?$", CLOUD},
+    {"^NSC$", NSC},
+    {"^NCD$", NCD},
+    {"^VV([0-9]{3})$", VERTICAL},
+    {"^(?:(M)?([0-9]{2})|\\/{2})\\/(?:(M)?([0-9]{2})|\\/{2})$", TEMPERATURE},
+    {"^Q(?:([0-9]{4})|\\/{4})$", QNH},
+    {"^WS$", WINDSHEAR},
+    {"^ALL$", ALL},
+    {"^RWY$", RWY},
+    {"^R([0-9]{2})$", RUNWAY_NUMBER},
 };
 
 #endif
